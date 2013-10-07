ae0d20262062e1e5388f0a244cfebb86aa006a22
marrcio <cubomarrcio@gmail.com>
2013-10-07 10:22:17 -0300

    Comments on FB IDs

--- appfoogle/static_files/js/facebookLogin.js
+++ appfoogle/static_files/js/facebookLogin.js
@@ -2,9 +2,9 @@
 var a;
 window.fbAsyncInit = function() {
   // init the FB JS SDK
-  FB.init({
-   // appId      : '394904207304456',                        // App ID from the app dashboard
-    appId       :  '336910786453085',
+  FB.init({ 
+   // appId      : '394904207304456',                        // ID for Deploy
+    appId       :  '336910786453085',                        // ID for local
     channelUrl : '//appfoogle.appspot.com/channel.html', // Channel file for x-domain comms
     status     : true,                                 // Check Facebook Login status
     xfbml      : true                                  // Look for social plugins on the page
