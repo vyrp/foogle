120b2cd7c5588e82f75882e02d6781ba3fe8c899
Filipe <luizmramos@gmail.com>
2013-10-07 11:05:53 -0300

    Facebook login button is now loading faster.

--- appfoogle/static_files/js/facebookLogin.js
+++ appfoogle/static_files/js/facebookLogin.js
@@ -11,6 +11,7 @@ window.fbAsyncInit = function() {
   });
   
   // Additional initialization code such as adding Event Listeners goes here
+  
 
   FB.Event.subscribe('auth.authResponseChange', function(response) {
   // Here we specify what we do with the response anytime this event occurs. 
@@ -18,6 +19,7 @@ window.fbAsyncInit = function() {
     // The response object is returned with a status field that lets the app know the current
     // login status of the person. In this case, we're handling the situation where they 
     // have logged in to the app.
+    console.log('connected');
     testAPI();
   } else if (response.status === 'not_authorized') {
     // In this case, the person is logged into Facebook, but not into the app, so we call
@@ -27,6 +29,7 @@ window.fbAsyncInit = function() {
     // (1) JavaScript created popup windows are blocked by most browsers unless they 
     // result from direct interaction from people using the app (such as a mouse click)
     // (2) it is a bad experience to be continually prompted to login upon page load.
+    console.log('not_authorized');
     FB.login();
   } else {
     // In this case, the person is not logged into Facebook, so we call the login() 
@@ -34,9 +37,11 @@ window.fbAsyncInit = function() {
     // of whether they are logged into the app. If they aren't then they'll see the Login
     // dialog right after they log in to Facebook. 
     // The same caveats as above apply to the FB.login() call here.
+    console.log('not logged');
     FB.login();
   }
 });
+
 
 };
 
