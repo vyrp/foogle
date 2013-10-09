bd5c7f933b4b7f0981f1f7213ac6b01dfb1ef7c6
Filipe <luizmramos@gmail.com>
2013-10-09 18:33:47 -0300

    Adding file with usefull facebook queries, initial database model and some simple handlers.

--- appfoogle/main.py
+++ appfoogle/main.py
@@ -15,11 +15,56 @@
 # limitations under the License.
 #
 import webapp2
+from google.appengine.ext import db
+
+
+
+class Comments(db.Model):
+	uid = db.IntegerProperty();
+	fbid = db.IntegerProperty();
+	word = db.StringProperty();
+	
+
+class Posts(db.Model):
+	uid = db.IntegerProperty();
+	fbidms = db.IntegerProperty();
+	fbidls = db.IntegerProperty();
+	word = db.StringProperty();
+	
+
+class Messages(db.Model):
+	uid = db.IntegerProperty();
+	fbid = db.IntegerProperty();
+	word = db.StringProperty();
+
+
+class GetFromCommentsHandler(webapp2.RequestHandler):
+	def get(self):
+		word=str(self.request.get('word'));
+		uid=str(self.request.get('uid'));
+		ocurrences = db.GqlQuery("SELECT fbid FROM Comments WHERE uid=" + uid + " AND word=\'" + word + "\'" );
+		response="";
+		for ocurrence in ocurrences:
+			response+=(str(ocurrence.fbid) + ",");
+		self.response.write(response[0:-1]	);
+		
+
+class DummyPutCommentHandler(webapp2.RequestHandler):
+	def get(self):
+		comment = Comments();
+		comment.fbid = int(self.request.get('fbid'));
+		comment.word = str(self.request.get('word'));
+		comment.uid = int(self.request.get('uid'));
+		comment.put();
+		self.response.write("Success");
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/putcomment', DummyPutCommentHandler),
+    ('/comments', GetFromCommentsHandler)    
 ], debug=True)
