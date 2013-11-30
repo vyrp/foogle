82e35d5732d0684203081d6c5c6c80e57899cdba
Filipe <luizmramos@gmail.com>
2013-11-30 21:58:34 -0200

    Adding simple search and multisearch.

--- appfoogle/main.py
+++ appfoogle/main.py
@@ -38,16 +38,17 @@ class Messages(ndb.Model):
 	word = ndb.StringProperty();
 
 
-class GetFromCommentsHandler(webapp2.RequestHandler):
-
-	def post(self):
+class SearchHandler(webapp2.RequestHandler):
+	def parseJson(self):
 		try:
 			body=json.loads(self.request.body)
 		except:
 			self.error(400)
 			self.response.write('400 invalid json in request body')
-			return
+			return None
+		return body
 
+	def getSearchParameters(self,body):
 		offset=None
 		if 'offset' in body:
 			offset=body['offset']
@@ -62,40 +63,74 @@ class GetFromCommentsHandler(webapp2.RequestHandler):
 			if not isinstance(limit,int):
 				limit=None
 		if not limit:
-			limit=20
-
-		if 'word' in body:
-			word=body['word']
-		else:
-			self.error(400)
-			self.response.write('400 invalid word')
-			return 
+			limit=20 
 
 		if 'uid' in body:
 			uid=body['uid']
 			if not isinstance(uid,int):
 				self.error(400)
 				self.response.write('400 non integer id')
-				return
+				return None
 		else:
 			self.error(400)
-			self.response.write('400 invalid uid')
-			return
+			self.response.write('400 missing uid')
+			return None
 
+		return {"offset":offset,"limit":limit,"uid":uid}
+
+	def getWord(self,body):
+		if 'word' in body:
+			word=body['word']
+		else:
+			self.error(400)
+			self.response.write('400 missing word')
+			return None
+		return word
+
+	def queryOcurrences(self,uid,word,offset,limit,cls):
 		try:
-			gqlOcurrences = Comments.query(ndb.AND(Comments.uid==int(uid),Comments.word==word))
+			gqlOcurrences = cls.query(ndb.AND(Comments.uid==int(uid),Comments.word==word))
 			ocurrences=gqlOcurrences.fetch(offset=offset,limit=limit)
 		except:
 			self.error(500)
 			self.response.write('500 error querying database')
-			return
+			return None
+		return ocurrences
 
-		fbids=[0]*len(ocurrences);
-		for i,ocurrence in enumerate(ocurrences):
-			fbids[i]=(ocurrence.fbid);
+	def processOcurrences(self):
+		pass
+
+	def search(self,cls,body=None,word=None):
+		if body==None:
+			body=self.parseJson()
+			if body==None:
+				return None
+
+		parameters=self.getSearchParameters(body)
+		if parameters==None:
+			return None
+		offset=parameters['offset'];
+		limit=parameters['limit'];
+		uid=parameters['uid'];
+
+		if word==None:
+			word=self.getWord(body);
+			if word==None:
+				return None
 		
+		ocurrences=self.queryOcurrences(uid=uid,word=word,offset=offset,limit=limit,cls=cls)
+		if ocurrences==None:
+			return None
+		fbids=self.processOcurrences(ocurrences)
+		fbids=[dict(y) for y in set(tuple(x.items()) for x in fbids)]
+		if fbids==None:
+			return None
+
 		response={'data':fbids}
-		self.response.write(json.dumps(response));
+		return response
+	
+
+
 		
 
 class PutCommentHandler(webapp2.RequestHandler):
@@ -118,22 +153,136 @@ class PutCommentHandler(webapp2.RequestHandler):
 			self.response.write('400 invalid words')
 			return 
 
-
 		response={'status':'success'}
 		self.response.write(json.dumps(response));
 
-class DummyPutCommentHandler(webapp2.RequestHandler):
-	def get(self):
-		comment = Comments();
-		comment.fbid = int(self.request.get('fbid'));
-		comment.word = str(self.request.get('word'));
-		comment.uid = int(self.request.get('uid'));
-		comment.put();
-		self.response.write("Success");
 
+class MultiSearchHandler(SearchHandler):
+	def getSentence(self,body):
+		if 'sentence' in body:
+			sentence=body['sentence']
+		else:
+			self.error(400)
+			self.response.write('400 missing sentence')
+			return None
+		return sentence
+
+	def normalize(self,word):
+		return word
+
+	def createFbidHash(self,el):
+		return None
+
+	def multi_search(self,cls):
+		body=self.parseJson()
+		if body==None:
+			return
+		sentence=self.getSentence(body)
+		if sentence==None:
+			return
+		words=[self.normalize(word) for word in sentence.split(" ")]
+		
+		results=[]
+		for word in words:
+			result=self.search(cls,body,word)
+			if result:
+				results.append(result)
+		
+		allOcurrences={}
+		for result in results:
+			if not 'data' in result:
+				continue
+			data=result['data']
+			for el in data:
+				fbid=self.createFbidHash(el)
+				if fbid==None:
+					continue
+				if not fbid in allOcurrences:
+					allOcurrences[fbid]=0
+				allOcurrences[fbid]+=1
+
+		sortedKeys=sorted(allOcurrences,key=allOcurrences.get,reverse=True)
+		
+		fbids=[{}]*len(sortedKeys);
+
+		for i,key in enumerate(sortedKeys):
+			fbids[i]=self.getObjectFromHash(key);
+			fbids[i]['rate']=allOcurrences[key];
+
+		response={'data':fbids}
+		return response
+
+class GetFromCommentsHandler(MultiSearchHandler):
+	def processOcurrences(self,ocurrences):
+		fbids=[0]*len(ocurrences);
+		for i,ocurrence in enumerate(ocurrences):
+			fbids[i]={'fbid':ocurrence.fbid}
+		return fbids
+
+	def createFbidHash(self,el):
+		if not 'fbid' in el:
+			return None
+		return el['fbid']
+
+	def getObjectFromHash(self,key):
+		return {'fbid':key}
+
+	def post(self):
+		response=self.multi_search(Comments)
+		if response==None:
+			return
+		self.response.write(json.dumps(response))
+
+
+
+class GetFromMessagesHandler(MultiSearchHandler):
+	def processOcurrences(self,ocurrences):
+		fbids=[0]*len(ocurrences);
+		for i,ocurrence in enumerate(ocurrences):
+			fbids[i]={'fbid':ocurrence.fbid}
+		return fbids
+
+	def createFbidHash(self,el):
+		if not 'fbid' in el:
+			return None
+		return el['fbid']
+
+	def getObjectFromHash(self,key):
+		return {'fbid':key}
+	
+	def post(self):
+		response=self.multi_search(Messages)
+		if response==None:
+			return
+		self.response.write(json.dumps(response))
+
+class GetFromPostsHandler(MultiSearchHandler):
+	def processOcurrences(self,ocurrences):
+		fbids=[0]*len(ocurrences);
+		for i,ocurrence in enumerate(ocurrences):
+			fbids[i]={'fbidl':ocurrence.fbidl,'fbidh':ocurrence.fbidh}
+		return fbids
+
+	def createFbidHash(self,el):
+		if not 'fbidl' in el:
+			return None
+		if not 'fbidh' in el:
+			return None
+		return str(el['fbidl'])+'_'+str(el['fbidh'])
+
+	def getObjectFromHash(self,key):
+		return {'fbid':key}
+
+	def post(self):
+		response=self.multi_search(Posts)
+		if response==None:
+			return
+		self.response.write(json.dumps(response))
 
 
 app = webapp2.WSGIApplication([
     ('/putcomment', PutCommentHandler),
-    ('/comments', GetFromCommentsHandler)    
+    ('/comments', GetFromCommentsHandler),
+    ('/messages', GetFromMessagesHandler),
+    ('/posts', GetFromPostsHandler)   
 ], debug=True)
