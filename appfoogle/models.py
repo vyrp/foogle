35f57fd6ddc53dc42910ddbe44a0f7a1beab562f
Croata <felipe.croata@gmail.com>
2013-12-01 09:33:06 -0200

    Refatoracao feita: passar para varios arquivos e remocao de arquivos nao mais utilizados.

---
+++ appfoogle/models.py
@@ -0,0 +1,34 @@
+from google.appengine.ext import ndb
+
+
+class Comments(ndb.Model):
+    uid = ndb.IntegerProperty();
+    fbid = ndb.StringProperty();
+    word = ndb.StringProperty();
+    timestamp = ndb.IntegerProperty();
+
+
+class Posts(ndb.Model):
+    uid = ndb.IntegerProperty();
+    fbid = ndb.StringProperty();
+    word = ndb.StringProperty();
+    timestamp = ndb.IntegerProperty();
+
+
+class Messages(ndb.Model):
+    uid = ndb.IntegerProperty();
+    fbid = ndb.StringProperty();
+    word = ndb.StringProperty();
+    timestamp = ndb.IntegerProperty();
+
+
+class User(ndb.Model):
+    uid = ndb.StringProperty(required=True)
+    access_token = ndb.StringProperty()
+    
+    @classmethod
+    def find_or_create(cls, uid):
+        user = cls.query(cls.uid == uid).get()
+        if not user:
+            user = User(uid=uid)
+        return user
